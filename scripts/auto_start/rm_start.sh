35d78365e6dc88c6d2f691ad7387c8ebbb7ea141
qiayuan <liaoqiayuan@gmail.com>
2021-04-09 05:20:17 +0800

    Replace /home/dynamicx using ~

--- scripts/auto_start/rm_start.sh
+++ scripts/auto_start/rm_start.sh
@@ -2,8 +2,8 @@
 sudo ip link set can0 up type can bitrate 1000000
 sudo ip link set can1 up type can bitrate 1000000
 source /opt/ros/noetic/setup.sh
-export ROS_PACKAGE_PATH=/home/dynamicx/rm_ws:$ROS_PACKAGE_PATH
-source /home/dynamicx/rm_ws/devel/setup.bash
+export ROS_PACKAGE_PATH=~/rm_ws:$ROS_PACKAGE_PATH
+source ~/rm_ws/devel/setup.bash
 export ROBOT_TYPE=sentry
 export ENEMY_COLOR=red
 export ROS_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1')
