b9ff3f4f352e7f3613e7361a0b2472f812d34b13
astro <1015926336@qq.com>
2021-04-08 15:19:51 +0800

    auto start without wifi

--- scripts/auto_start/rm_start.sh
+++ scripts/auto_start/rm_start.sh
@@ -6,7 +6,8 @@ export ROS_PACKAGE_PATH=/home/dynamicx/rm_ws:$ROS_PACKAGE_PATH
 source /home/dynamicx/rm_ws/devel/setup.bash
 export ROBOT_TYPE=sentry
 export ENEMY_COLOR=red
-export ROS_IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1'`
+export ROS_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1')
+if test -z "${ROS_IP}"; then
+  export ROS_IP=$(dig +short localhost)
+fi
 roslaunch rm_bringup start.launch
-
-
