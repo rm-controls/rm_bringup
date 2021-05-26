fed8ba769e50aaf8c214ac9a8563b67f7f8c4c16
huakang-402 <1003970864@qq.com>
2021-05-26 20:48:04 +0800

    Add start master service and use mon replace roslaunch

--- scripts/auto_start/create_rm_start_service.sh
+++ scripts/auto_start/create_rm_start_service.sh
@@ -2,14 +2,18 @@
 echo " "
 echo "Start to copy script files to /home/dynamicx"
 echo ""
+cp `rospack find rm_bringup`/scripts/auto_start/start_master.sh ~/
 cp `rospack find rm_bringup`/scripts/auto_start/rm_start.sh ~/
+chmod 777 ~/start_master.sh
 chmod 777 ~/rm_start.sh
 echo " "
 echo "Start to copy service files to /lib/systemd/system/"
 echo ""
+sudo cp `rospack find rm_bringup`/scripts/auto_start/start_master.service  /lib/systemd/system/
 sudo cp `rospack find rm_bringup`/scripts/auto_start/rm_start.service  /lib/systemd/system/
 echo " "
 echo "Enable auto rm start! "
 echo ""
+sudo systemctl enable start_master.service
 sudo systemctl enable rm_start.service
 echo "Finish "
--- scripts/auto_start/start_master.sh
+++ scripts/auto_start/start_master.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-sudo ip link set can0 up type can bitrate 1000000
-sudo ip link set can1 up type can bitrate 1000000
 source /opt/ros/noetic/setup.bash
 export ROS_PACKAGE_PATH=~/rm_ws:$ROS_PACKAGE_PATH
 source ~/rm_ws/devel/setup.bash
-export ROBOT_TYPE=sentry
-export ENEMY_COLOR=red
 export ROS_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1')
 if test -z "${ROS_IP}"; then
   export ROS_IP=$(dig +short localhost)
 fi
-mon launch --disable-ui rm_bringup start.launch
-
+roscore
