9d670e76ea1551e4ded47c5f946325601f0069ef
chenzheng <1047979458@qq.com>
2021-05-29 22:48:26 +0800

    Add init can2

--- scripts/auto_start/AutoSetCan.sh
+++ scripts/auto_start/AutoSetCan.sh
@@ -3,5 +3,6 @@ while true
 do
     sudo ip link set can0 up type can bitrate 1000000
     sudo ip link set can1 up type can bitrate 1000000
+    sudo ip link set can2 up type can bitrate 1000000
     sleep 1
 done
