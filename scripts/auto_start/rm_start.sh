2b854a67a56604f824b413ba3d4bf1f7d3d274b2
BruceLannn <1656257449@qq.com>
2021-07-26 07:00:05 +0800

    Add CAMERA_TYPE into rm_start.sh

--- scripts/auto_start/rm_start.sh
+++ scripts/auto_start/rm_start.sh
@@ -5,7 +5,8 @@ sudo ip link set can2 up type can bitrate 1000000
 source /opt/ros/noetic/setup.bash
 export ROS_PACKAGE_PATH=~/rm_ws:$ROS_PACKAGE_PATH
 source ~/rm_ws/devel/setup.bash
-export ROBOT_TYPE=sentry
+export ROBOT_TYPE=hero
+export CAMERA_TYPE=mv_camer
 export ENEMY_COLOR=red
 export ROS_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1')
 if test -z "${ROS_IP}"; then
