3f6a6f5fb6acc17874cfea0128d65fd381125b78
Jiachen Shen <1836914000@qq.com>
2021-07-29 12:11:54 +0000

    Update rm_start.sh

--- scripts/auto_start/rm_start.sh
+++ scripts/auto_start/rm_start.sh
@@ -6,7 +6,7 @@ source /opt/ros/noetic/setup.bash
 export ROS_PACKAGE_PATH=~/rm_ws:$ROS_PACKAGE_PATH
 source ~/rm_ws/devel/setup.bash
 export ROBOT_TYPE=hero
-export CAMERA_TYPE=mv_camer
+export CAMERA_TYPE=mv_camera
 export ENEMY_COLOR=red
 export ROS_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1')
 if test -z "${ROS_IP}"; then
