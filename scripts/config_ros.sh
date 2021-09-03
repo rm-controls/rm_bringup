9458b531c207de0c87d09d83b488b146b5366cac
BruceLannn <1656257449@qq.com>
2021-09-03 18:13:32 +0800

    Add per-commit and correct format.

--- scripts/config_ros.sh
+++ scripts/config_ros.sh
@@ -1,7 +1,7 @@
 
 export ROS_IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 #export ROS_MASTER_URI=http://192.168.1.141:11311
-echo "ROS IP: ${ROS_IP} 
+echo "ROS IP: ${ROS_IP}
 ROS Master URI: ${ROS_MASTER_URI}"
 
 export ROS_PACKAGE_PATH=~/Project/rm-software/robot_ws:$ROS_PACKAGE_PATH
