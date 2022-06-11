996113b1ee389be1069c91bb0780c24048ad8205
ljq-lv <939468378@qq.com>
2022-06-12 03:30:33 +0800

    Update gpio's pin

--- scripts/auto_start/auto_set_gpio.sh
+++ scripts/auto_start/auto_set_gpio.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
+while true
+do
 sudo modprobe gpio-pcf857x
 sudo sh -c 'echo pcf8574 0x20 > /sys/bus/i2c/devices/i2c-15/new_device'
 sudo chmod 777 /sys/class/gpio/export
 sudo chmod 777 /sys/class/gpio/unexport
-echo 144 > /sys/class/gpio/export
-echo 145 > /sys/class/gpio/export
-echo 146 > /sys/class/gpio/export
-echo 147 > /sys/class/gpio/export
-echo 148 > /sys/class/gpio/export
-echo 149 > /sys/class/gpio/export
-echo 150 > /sys/class/gpio/export
-echo 151 > /sys/class/gpio/export
-sudo chmod 777 /sys/class/gpio/gpio144/direction
-sudo chmod 777 /sys/class/gpio/gpio145/direction
-sudo chmod 777 /sys/class/gpio/gpio146/direction
-sudo chmod 777 /sys/class/gpio/gpio147/direction
-sudo chmod 777 /sys/class/gpio/gpio148/direction
-sudo chmod 777 /sys/class/gpio/gpio149/direction
-sudo chmod 777 /sys/class/gpio/gpio150/direction
-sudo chmod 777 /sys/class/gpio/gpio151/direction
-sudo chmod 777 /sys/class/gpio/gpio144/value
-sudo chmod 777 /sys/class/gpio/gpio145/value
-sudo chmod 777 /sys/class/gpio/gpio146/value
-sudo chmod 777 /sys/class/gpio/gpio147/value
-sudo chmod 777 /sys/class/gpio/gpio148/value
-sudo chmod 777 /sys/class/gpio/gpio149/value
-sudo chmod 777 /sys/class/gpio/gpio150/value
-sudo chmod 777 /sys/class/gpio/gpio151/value
+echo 656 > /sys/class/gpio/export
+echo 657 > /sys/class/gpio/export
+echo 658 > /sys/class/gpio/export
+echo 659 > /sys/class/gpio/export
+echo 660 > /sys/class/gpio/export
+echo 661 > /sys/class/gpio/export
+echo 662 > /sys/class/gpio/export
+echo 663 > /sys/class/gpio/export
+sudo chmod 777 /sys/class/gpio/gpio656/direction
+sudo chmod 777 /sys/class/gpio/gpio657/direction
+sudo chmod 777 /sys/class/gpio/gpio658/direction
+sudo chmod 777 /sys/class/gpio/gpio659/direction
+sudo chmod 777 /sys/class/gpio/gpio660/direction
+sudo chmod 777 /sys/class/gpio/gpio661/direction
+sudo chmod 777 /sys/class/gpio/gpio662/direction
+sudo chmod 777 /sys/class/gpio/gpio663/direction
+sudo chmod 777 /sys/class/gpio/gpio656/value
+sudo chmod 777 /sys/class/gpio/gpio657/value
+sudo chmod 777 /sys/class/gpio/gpio658/value
+sudo chmod 777 /sys/class/gpio/gpio659/value
+sudo chmod 777 /sys/class/gpio/gpio660/value
+sudo chmod 777 /sys/class/gpio/gpio661/value
+sudo chmod 777 /sys/class/gpio/gpio662/value
+sudo chmod 777 /sys/class/gpio/gpio663/value
