4bc4293d5825df7ea21dde43f25bb73a6dd6df91
LHTcode <LHT13535837097@163.com>
2022-07-12 17:00:31 +0800

    Modify the process name that memory_monitor.sh will kill.

--- scripts/auto_start/memory_monitor.sh
+++ scripts/auto_start/memory_monitor.sh
@@ -4,7 +4,7 @@ do
         temp=$(df -H | grep -vE '^Filesystem|tmpfs|udev' | grep -E "^/dev.*/$" | awk '{ print $5}')
         result=$(echo "$temp" | sed -E 's/[^0-9]//g')
         if [[ $result -ge '90' ]]; then
-                temp=$(ps -ef | grep -E "^.*/bin/bash.*rosbag_record_service.sh$")
+                temp=$(ps -ef | grep -E "^.*/bin/bash.*camera_rosbag_record_service.sh$")
                 id=$(echo "$temp" | sed -E 's/^\S+\s+([0-9]+).*$/\1/g')
                 echo "$id"
                 kill "$id"
