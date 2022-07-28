d10ed1bfa5d4c5498a5f5270309123dbd179c842
ljq-lv <939468378@qq.com>
2022-07-28 20:13:31 +0800

    Modified the struct of auto_set_gpio.sh

--- scripts/auto_start/auto_set_gpio.sh
+++ scripts/auto_start/auto_set_gpio.sh
@@ -1,32 +1,11 @@
 #!/bin/bash
-while true
-do
 sudo modprobe gpio-pcf857x
 sudo sh -c 'echo pcf8574 0x20 > /sys/bus/i2c/devices/i2c-15/new_device'
 sudo chmod 777 /sys/class/gpio/export
 sudo chmod 777 /sys/class/gpio/unexport
-echo 656 > /sys/class/gpio/export
-echo 657 > /sys/class/gpio/export
-echo 658 > /sys/class/gpio/export
-echo 659 > /sys/class/gpio/export
-echo 660 > /sys/class/gpio/export
-echo 661 > /sys/class/gpio/export
-echo 662 > /sys/class/gpio/export
-echo 663 > /sys/class/gpio/export
-sudo chmod 777 /sys/class/gpio/gpio656/direction
-sudo chmod 777 /sys/class/gpio/gpio657/direction
-sudo chmod 777 /sys/class/gpio/gpio658/direction
-sudo chmod 777 /sys/class/gpio/gpio659/direction
-sudo chmod 777 /sys/class/gpio/gpio660/direction
-sudo chmod 777 /sys/class/gpio/gpio661/direction
-sudo chmod 777 /sys/class/gpio/gpio662/direction
-sudo chmod 777 /sys/class/gpio/gpio663/direction
-sudo chmod 777 /sys/class/gpio/gpio656/value
-sudo chmod 777 /sys/class/gpio/gpio657/value
-sudo chmod 777 /sys/class/gpio/gpio658/value
-sudo chmod 777 /sys/class/gpio/gpio659/value
-sudo chmod 777 /sys/class/gpio/gpio660/value
-sudo chmod 777 /sys/class/gpio/gpio661/value
-sudo chmod 777 /sys/class/gpio/gpio662/value
-sudo chmod 777 /sys/class/gpio/gpio663/value
-Adone
+for i in 656 657 658 659 660 661 662 663
+do
+echo ${i} > /sys/class/gpio/export
+sudo chmod 777 /sys/class/gpio/gpio${i}/direction
+sudo chmod 777 /sys/class/gpio/gpio${i}/value
+done
