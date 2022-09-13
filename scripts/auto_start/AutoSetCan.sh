c0d2d5d7ff9d1efc98b0576472cdb99ad7c7b031
caijiefeng <2726784110@qq.com>
2022-09-13 20:42:54 +0800

    Updated drone's bringup.

--- scripts/auto_start/AutoSetCan.sh
+++ scripts/auto_start/AutoSetCan.sh
@@ -2,7 +2,5 @@
 while true
 do
     sudo ip link set can0 up type can bitrate 1000000
-    sudo ip link set can1 up type can bitrate 1000000
-    sudo ip link set can2 up type can bitrate 1000000
     sleep 1
 done
