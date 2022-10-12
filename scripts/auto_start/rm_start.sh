0bbc438c1d3eb8bb448dc29602900a3fd7ee016d
caijiefeng <2726784110@qq.com>
2022-10-12 11:20:11 +0800

    Updated drone's bringup scripts.

--- scripts/auto_start/rm_start.sh
+++ scripts/auto_start/rm_start.sh
@@ -4,5 +4,9 @@ export ROS_PACKAGE_PATH=~/rm_ws:$ROS_PACKAGE_PATH
 source ~/rm_ws/devel/setup.bash
 export ROBOT_TYPE=drone
 export ENEMY_COLOR=red
+export ROS_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1')
+if test -z "${ROS_IP}"; then
+  export ROS_IP=$(dig +short localhost)
+fi
 mon launch --disable-ui rm_bringup start.launch
 
