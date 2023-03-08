2c815d6747fd727b20f44d54b63a611e39891be7
yezi <1536117624@qq.com>
2023-03-08 14:28:42 +0800

    Update auto start script.

--- scripts/auto_start/rm_start.sh
+++ scripts/auto_start/rm_start.sh
@@ -7,7 +7,8 @@ export ROS_PACKAGE_PATH=~/rm_ws:$ROS_PACKAGE_PATH
 source ~/rm_ws/devel/setup.bash
 source /opt/intel/openvino_2021/bin/setupvars.sh
 export ROBOT_TYPE=hero
-export CAMERA_TYPE=mv_camera
+export CAMERA_TYPE=hk_camera
+export CAMERA_CLASS=HKCameraNodelet
 export ENEMY_COLOR=red
 export ROS_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1')
 if test -z "${ROS_IP}"; then
