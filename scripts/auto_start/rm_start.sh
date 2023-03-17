69e14bccb0f56445cf062885aaf2acb382016377
yezi <1536117624@qq.com>
2023-03-18 00:27:03 +0800

    Add hk_camera environment.

--- scripts/auto_start/rm_start.sh
+++ scripts/auto_start/rm_start.sh
@@ -9,6 +9,12 @@ source /opt/intel/openvino_2021/bin/setupvars.sh
 export ROBOT_TYPE=hero
 export CAMERA_TYPE=hk_camera
 export CAMERA_CLASS=HKCameraNodelet
+
+export MVCAM_SDK_PATH=/opt/MVS
+
+export MVCAM_COMMON_RUNENV=/opt/MVS/lib
+export LD_LIBRARY_PATH=/opt/MVS/lib/64:/opt/MVS/lib/32:$LD_LIBRARY_PATH
+
 export ENEMY_COLOR=red
 export ROS_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1')
 if test -z "${ROS_IP}"; then
