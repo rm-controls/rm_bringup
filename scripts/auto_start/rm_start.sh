cd0f322876891c8fdd77d11c57978ddc85782f7f
yezi <1536117624@qq.com>
2023-04-10 12:32:08 +0800

    Solve the problem that miniPC can not get ip when it boot.

--- scripts/auto_start/rm_start.sh
+++ scripts/auto_start/rm_start.sh
@@ -16,9 +16,21 @@ export MVCAM_COMMON_RUNENV=/opt/MVS/lib
 export LD_LIBRARY_PATH=/opt/MVS/lib/64:/opt/MVS/lib/32:$LD_LIBRARY_PATH
 
 export ENEMY_COLOR=red
+ip="192.168.1.103"
 export ROS_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1')
+for i in {1..10}
+do
+    if [ "$ROS_IP" = "$ip" ]
+    then
+        break
+    else
+        sleep 1
+        ROS_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1')
+    fi
+done
 if test -z "${ROS_IP}"; then
   export ROS_IP=$(dig +short localhost)
 fi
+export ROS_MASTER_URI=http://$ROS_IP:11311
 mon launch --disable-ui rm_bringup start.launch
 
--- scripts/auto_start/start_master.sh
+++ scripts/auto_start/start_master.sh
@@ -2,8 +2,20 @@
 source /opt/ros/noetic/setup.bash
 export ROS_PACKAGE_PATH=~/rm_ws:$ROS_PACKAGE_PATH
 source ~/rm_ws/devel/setup.bash
+ip="192.168.1.103"
 export ROS_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1')
+for i in {1..10}
+do
+    if [ "$ROS_IP" = "$ip" ]
+    then
+        break
+    else
+        sleep 1
+        ROS_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.17.0.1')
+    fi
+done
 if test -z "${ROS_IP}"; then
   export ROS_IP=$(dig +short localhost)
 fi
+export ROS_MASTER_URI=http://$ROS_IP:11311
 roscore
