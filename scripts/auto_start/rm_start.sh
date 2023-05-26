ea481bd2f339a2ba4708d0cf3ecfe0ce0f441a1c
yezi <1536117624@qq.com>
2023-05-27 01:59:04 +0800

    Separate environment variables.

--- scripts/auto_start/rm_start.sh
+++ scripts/auto_start/rm_start.sh
@@ -6,16 +6,10 @@ source /opt/ros/noetic/setup.bash
 export ROS_PACKAGE_PATH=~/rm_ws:$ROS_PACKAGE_PATH
 source ~/rm_ws/devel/setup.bash
 source /opt/intel/openvino_2021/bin/setupvars.sh
-export ROBOT_TYPE=standard5
-export CAMERA_TYPE=hk_camera
-export CAMERA_CLASS=HKCameraNodelet
-
-export MVCAM_SDK_PATH=/opt/MVS
-
-export MVCAM_COMMON_RUNENV=/opt/MVS/lib
-export LD_LIBRARY_PATH=/opt/MVS/lib/64:/opt/MVS/lib/32:$LD_LIBRARY_PATH
-
-export ENEMY_COLOR=red
-export ROS_IP=192.168.100.2
-mon launch --disable-ui rm_bringup start.launch
-
+source ./environment.sh
+if [[ $HAS_SWITCH == has ]]; then
+  export ROS_IP=192.168.100.2
+else
+  export ROS_IP=127.0.0.1
+fi
+mon launch --disable-ui rm_bringup $LAUNCH.launch
