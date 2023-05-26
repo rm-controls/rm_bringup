56a5c820b4bb7c3f37b883b740ef0c858d68cddf
yezi <1536117624@qq.com>
2023-05-27 04:42:27 +0800

    The relative path is changed to the absolute path.

--- scripts/auto_start/start_master.sh
+++ scripts/auto_start/start_master.sh
@@ -2,7 +2,7 @@
 source /opt/ros/noetic/setup.bash
 export ROS_PACKAGE_PATH=~/rm_ws:$ROS_PACKAGE_PATH
 source ~/rm_ws/devel/setup.bash
-source ./environment.sh
+source ~/environment.sh
 if [[ $HAS_SWITCH == has ]]; then
   export ROS_IP=192.168.100.2
 else
