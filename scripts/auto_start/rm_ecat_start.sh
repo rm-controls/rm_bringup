d687b798875d61df2a7b54dec024af298d78b32c
BruceLannn <1656257449@qq.com>
2023-06-10 23:54:25 +0800

    Add ethercat2xxx auto start service and shell

---
+++ scripts/auto_start/rm_ecat_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source /opt/ros/noetic/setup.bash
+source /home/dynamicx/rm_ws/devel/setup.bash
+source /home/dynamicx/environment.sh
+if [[ $HAS_SWITCH == has ]]; then
+  export ROS_IP=192.168.100.2
+else
+  export ROS_IP=127.0.0.1
+fi
+mon launch --disable-ui rm_bringup $LAUNCH.launch
