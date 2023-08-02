b1077acf455a9a014239872a82a63003857e2819
BruceLannn <1656257449@qq.com>
2023-08-02 23:00:57 +0800

    Add balance param for ecat_hw

--- scripts/auto_start/rm_ecat_start.sh
+++ scripts/auto_start/rm_ecat_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.bash
-source /home/dynamicx/rm_ws/devel/setup.bash
+source /home/dynamicx/mpc_ws/devel/setup.bash
 source /home/dynamicx/environment.sh
 if [[ $HAS_SWITCH == has ]]; then
   export ROS_IP=192.168.100.2
